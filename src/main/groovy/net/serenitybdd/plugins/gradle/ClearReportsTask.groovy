164585be3481383d8532fa90e2b42b3502b23e21
John Ferguson Smart <john.smart@wakaleo.com>
2024-04-19 07:15:13 +0100

    Removed deprecated tasks and fixed compile issues with the Clear Reports task

--- src/main/groovy/net/serenitybdd/plugins/gradle/ClearReportsTask.groovy
+++ src/main/groovy/net/serenitybdd/plugins/gradle/ClearReportsTask.groovy
@@ -1,14 +1,25 @@
 package net.serenitybdd.plugins.gradle
 
 import org.apache.commons.io.FileUtils
+import org.gradle.api.file.ProjectLayout
+import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.TaskAction
+
+import javax.inject.Inject
+import java.nio.file.Path
 
 class ClearReportsTask extends SerenityAbstractTask {
 
+    @Inject
+    ClearReportsTask(ProjectLayout layout) {
+        super(layout)
+    }
+
+    @InputDirectory
+    abstract Path reportDirectory;
+
     @TaskAction
     void clearReports() {
-        SerenityAbstractTask.updateProperties(project)
-        def reportDirectory = SerenityAbstractTask.prepareReportDirectory(project)
         FileUtils.deleteDirectory(reportDirectory.toFile())
     }
 
